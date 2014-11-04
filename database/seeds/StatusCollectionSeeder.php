55873ee4d93174cf42639c68d850c62eb912f522
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2014-11-04 10:35:18 +0630

    fix api key

--- database/seeds/StatusCollectionSeeder.php
+++ database/seeds/StatusCollectionSeeder.php
@@ -13,7 +13,7 @@ class StatusCollectionSeeder extends Seeder {
         for ($count=0; $count < 8 ; $count++) {
             Status::create([
                 'user_id' => $faker->numberBetween($min = 1, $max = 3),
-                'place_id' => $faker->numberBetween($min = 1, $max = 3),
+                'location_id' => $faker->numberBetween($min = 1, $max = 3),
                 'status'  => $faker->numberBetween($min = 1000, $max = 1005),
                 'comment' => $faker->text($maxNbChars = 100)
             ]);
