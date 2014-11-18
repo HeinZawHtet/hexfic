d52c6f4243225cabd27a9cd89b34838c8b2c2045
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2014-11-18 16:52:40 +0630

    fix method not found error

--- app/Transformers/Transformer.php
+++ app/Transformers/Transformer.php
@@ -1,10 +1,10 @@
 <?php namespace App\Transformers;
 
 abstract class Transformer {
-	// public function transformCollection(array $items)
-	// {
-	// 	return array_map([$this, 'transform'], $items);
-	// }
+	public function transformCollection(array $items)
+	{
+		return array_map([$this, 'transform'], $items);
+	}
 
 	public function transformByLocation(array $items)
 	{
